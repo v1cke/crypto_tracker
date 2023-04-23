c76b63571e5680d6846650312f36494f04523859
v1cke <v1cke1982@gmail.com>
2023-04-23 23:11:22 +0200

    chart included

--- src/app/crypto-prices/crypto-prices.component.ts
+++ src/app/crypto-prices/crypto-prices.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { TrackerService } from '../tracker.service';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Chart, registerables } from 'chart.js/auto';
+Chart.register(...registerables);
 
-export interface PeriodicElement {
-  position: number;
-  name: string;
-  price: number;
-  change24: number;
-  change7: number;
-  cap: number;
-  volume: number;
-  supply: number;
-}
+Injectable({
+  providedIn: 'root'
+})
+
+// export interface PeriodicElement {
+//   position: number;
+//   name: string;
+//   price: number;
+//   change24: number;
+//   change7: number;
+//   cap: number;
+//   volume: number;
+//   supply: number;
+// }
 
 @Component({
   selector: 'app-crypto-prices',
@@ -19,18 +26,46 @@ export interface PeriodicElement {
 })
 export class CryptoPricesComponent implements OnInit {
 
-  public newdata: any = [];
-  public logos: any;
-  displayedColumns: string[] = ['position', 'name', 'price', 'change24', 'change7', 'cap', 'volume', 'supply'];
+  public url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=24h%2C%207d&locale=de';
+  public coins: any = [];
+  public chartsData: any = [];
+  public coinUrl: any = [];
 
-  constructor(private service: TrackerService) { }
+  constructor(private http: HttpClient) { }
+
+  public getdata() {
+    return this.http.get(this.url);
+  }
 
   async ngOnInit() {
-    await this.service.getdata().subscribe(res => {
-      this.newdata = res;
-      console.log(this.newdata);
+    await this.getdata().subscribe(res => {
+      this.coins = res;
+      console.log(this.coins);
+      setTimeout(() => {
+        this.createChart();
+      }, 3000);
+    });
+  }
+
+  async createChart() {
+    await this.coins.forEach((coin) => {
+      let i = this.coins.indexOf(coin);
+      console.log(coin);
+      this.addChart(coin, i);
+    })
+  }
+
+  async addChart(coin, i) {
+    new Chart(("myChart" + i), {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: '',
+          data: [coin.sparkline_in_7d.price]
+        }]
+      },
+      options: { 
+      }
     });
   }
 }
-
-
--- src/app/tracker.service.ts
+++ src/app/tracker.service.ts
@@ -5,12 +5,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TrackerService {
-  public url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d&locale=de';
 
   constructor(private http:HttpClient) { }
 
   public getdata(){
-    return this.http.get(this.url);
   }
   
 }
